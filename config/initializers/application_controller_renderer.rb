a7d36761ca787ec6beede3768893d7bd6c71bb33
かずき <skaz0110@yahoo.co.jp>
2020-11-22 09:57:48 +0900

    ユーザー一覧実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '95c06b9560cffcc88fc8e69f1de2d9719823bd0bdb8b094905f0d8f5394fb052d93b183d37c9b8744441bf3118551ff57ff81c2826ab525424b080af89084acd'
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_131948) do
+ActiveRecord::Schema.define(version: 2020_11_21_091521) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encryp ted_password", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
