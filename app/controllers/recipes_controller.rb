fc96fbe00caf145d6bac269c7e864738abb809f8
かずき <skaz0110@yahoo.co.jp>
2020-11-22 18:35:04 +0900

    レシピ管理の実装完了

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,47 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!,except: [:index]
+  
   def index
+    @recipes = Recipe.all
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def new
+    @recipe = Recipe.new
   end
 
-  def edit
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id =current_user.id
+    @recipe.save
+    redirect_to recipe_path(@recipe)
   end
+  
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+  
+
+  private 
+    def recipe_params
+      params.require(:recipe).permit(:title,:body,:image)
+    end
+  
+  
+
 end
